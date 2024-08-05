9648c3f594e80032e9862e1da60d9b66f3d9f0a8
ecabrera01 <ecabrera@trainingroots.xyz>
2024-08-04 18:32:23 -0600

    Add service and controller to know the status of the bot

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { TelegramReminderBotService } from './telegram-reminder-bot/telegram-reminder-bot.service';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+
 
 @Module({
   imports: [ConfigModule.forRoot()],
   providers: [AppService, TelegramReminderBotService],
+  controllers: [AppController],
 })
 export class AppModule {}
